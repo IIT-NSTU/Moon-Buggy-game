910881190c89a1cd14764493b0d60856f35de0e8
Yarafat2001 <121226676+Yarafat2001@users.noreply.github.com>
2023-08-28 15:00:33 +0600

    Update MoonBuggyGame.java

--- MoonBuggyGame.java
+++ MoonBuggyGame.java
@@ -3,25 +3,25 @@ import java.util.*;
 class MoonBuggyGame {
     Scanner scanner = new Scanner(System.in);
 
-    boolean gameOver;
+    boolean game_over;
     int distance;
     int energy;
-    int gridSize = 10;
+    int grid_size = 10;
     Vehicle[] players = new Vehicle[2];
     List<Enemy> enemies = new ArrayList<>();
-    MoonSurface moonSurface;
+    Moon_surface moonSurface;
 
     void playGame() {
-        System.out.println("Welcome to Moon Buggy Multiplayer");
+        System.out.println("Welcome to Moon Buggy ");
         System.out.println("Choose a game mode:");
         System.out.println("1. Player vs Player");
         System.out.println("2. Player vs Computer");
-        int gameMode = scanner.nextInt();
+        int game_mode = scanner.nextInt();
 
-        if (gameMode == 1) {
-            initializePlayers(2);
-             } else if (gameMode == 2) {
-            initializePlayers(1);
+        if (game_mode == 1) {
+            init_players(2);
+             } else if (game_mode == 2) {
+            init_players(1);
             players[1] = new ComputerPlayer();
             } else {
             System.out.println("Invalid game mode selection. Exiting the game.");
@@ -30,14 +30,14 @@ class MoonBuggyGame {
 
         energy = 50; 
 
-        initializeEnemies();
+        init_enemies();
 
-        moonSurface = new MoonSurface(gridSize);
+        moonSurface = new Moon_surface(grid_size);
 
-        while (!gameOver) {
+        while (!game_over) {
             for (Vehicle player : players) {
                 playTurn(player);
-                if (gameOver) {
+                if (game_over) {
                     break;
                 }
             }
@@ -46,17 +46,17 @@ class MoonBuggyGame {
         System.out.println("Game Over. Thanks for playing!");
     }
 
-    void initializePlayers(int numPlayers) {
-        for (int i = 0; i < numPlayers; i++) {
+    void init_players(int num_players) {
+        for (int i = 0; i < num_players; i++) {
             System.out.print("Enter name for Player " + (i + 1) + ": ");
-            String playerName = scanner.next();
-            players[i] = new Vehicle(playerName);
+            String player_name = scanner.next();
+            players[i] = new Vehicle(player_name);
         }
     }
 
     void playTurn(Vehicle player) {
-        displayGameState(player);
-        moonSurface.display(player.getPositionX(), player.getPositionY(), getEnemyPositions());
+        display_game_state(player);
+        moonSurface.display(player.get_posX(), player.get_posY(), get_enemy_pos());
 
         System.out.print(player.getName() + ", enter 'F' to move forward, 'B' to move backward, 'U' to move up, 'D' to move down, 'E' to make enemies escape, 'Q' to quit: ");
         String input = scanner.next();
@@ -74,10 +74,10 @@ class MoonBuggyGame {
                 movePlayer(player, 0, 1);
                 break;
             case "E":
-                escapeEnemies();
+                escape_enemies();
                 break;
             case "Q":
-                gameOver = true;
+                game_over = true;
                 break;
             default:
                 System.out.println("Invalid input.");
@@ -86,34 +86,34 @@ class MoonBuggyGame {
 
         updateGame(player);
         enemyAttack(player);
-        checkCollision(player);
+        check_collis(player);
     }
 
-    void initializeEnemies() {
+    void init_enemies() {
         Random random = new Random();
-        int numEnemies = random.nextInt(3) + 2;
+        int num_enemies = random.nextInt(3) + 2;
 
-        for (int i = 0; i < numEnemies; i++) {
-            int enemyPositionX = random.nextInt(gridSize);
-            int enemyPositionY = random.nextInt(gridSize);
-            enemies.add(new Enemy(enemyPositionX, enemyPositionY));
+        for (int i = 0; i < num_enemies; i++) {
+            int enemy_posX = random.nextInt(grid_size);
+            int enemy_posY = random.nextInt(grid_size);
+            enemies.add(new Enemy(enemy_posX, enemy_posX));
         }
     }
 
-    int[] getEnemyPositions() {
-        int[] enemyPositions = new int[enemies.size() * 2];
+    int[] get_enemy_pos() {
+        int[] enemy_pos = new int[enemies.size() * 2];
         for (int i = 0; i < enemies.size(); i++) {
-            enemyPositions[i * 2] = enemies.get(i).getPositionX();
-            enemyPositions[i * 2 + 1] = enemies.get(i).getPositionY();
+            enemy_pos[i * 2] = enemies.get(i).get_posX();
+            enemy_pos[i * 2 + 1] = enemies.get(i).get_posY();
         }
-        return enemyPositions;
+        return enemy_pos;
     }
 
     void movePlayer(Vehicle player, int directionX, int directionY) {
-        int newPositionX = player.getPositionX() + directionX;
-        int newPositionY = player.getPositionY() + directionY;
-        if (newPositionX >= 0 && newPositionX < gridSize && newPositionY >= 0 && newPositionY < gridSize) {
-            player.setPosition(newPositionX, newPositionY);
+        int new_posX = player.get_posX() + directionX;
+        int new_posY = player.get_posY() + directionY;
+        if (new_posX >= 0 && new_posX < grid_size && new_posY >= 0 && new_posY < grid_size) {
+            player.set_pos(new_posX, new_posY);
             distance++;
         } else {
             System.out.println("Can't move there.");
@@ -121,21 +121,21 @@ class MoonBuggyGame {
     }
 
     void updateGame(Vehicle player) {
-        if (distance >= gridSize) {
+        if (distance >= grid_size) {
             if (energy > 0) {
                 System.out.println("Congratulations! " + player.getName() + " wins!");
             } else {
                 System.out.println(player.getName() + " has reached the goal, but they are out of energy.");
             }
-            gameOver = true;
+            game_over = true;
         }
         if (energy <= 0) {
             System.out.println(player.getName() + " is out of energy.");
-            gameOver = true;
+            game_over = true;
         }
     }
 
-    void escapeEnemies() {
+    void escape_enemies() {
         for (Enemy enemy : enemies) {
             enemy.escape();
         }
@@ -148,25 +148,25 @@ class MoonBuggyGame {
         }
     }
 
-    void checkCollision(Vehicle player) {
+    void check_collis(Vehicle player) {
         for (Enemy enemy : enemies) {
-            if (enemy.getPositionX() == player.getPositionX() && enemy.getPositionY() == player.getPositionY()) {
+            if (enemy.get_posX() == player.get_posX() && enemy.get_posY() == player.get_posY()) {
                 System.out.println("Collision! " + enemy.getName() + " attacked " + player.getName() + "!");
                 energy -= 10;
-                moonSurface.markCollision(player.getPositionX(), player.getPositionY());
+                moonSurface.mark_collis(player.get_posX(), player.get_posY());
             }
         }
     }
 
-    void displayGameState(Vehicle player) {
+    void display_game_state(Vehicle player) {
         System.out.println("Distance: " + distance);
         System.out.println("Energy: " + energy);
-        System.out.println(player.getName() + " position: (" + player.getPositionX() + ", " + player.getPositionY() + ")");
+        System.out.println(player.getName() + " position: (" + player.get_posX() + ", " + player.get_posY() + ")");
     }
 
-    void displayWinners() {
+    void display_winners() {
         for (Vehicle player : players) {
-            if (player.getPositionX() >= gridSize) {
+            if (player.get_posX() >= grid_size) {
                 if (energy > 0) {
                     System.out.println("Congratulations! " + player.getName() + " wins!");
                 } else {
