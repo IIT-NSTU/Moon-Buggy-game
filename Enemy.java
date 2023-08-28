8955e871989ae0c3c4f4f41b7f2d45292cf7a0be
Yarafat2001 <121226676+Yarafat2001@users.noreply.github.com>
2023-08-28 15:01:22 +0600

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,30 +1,30 @@
 class Enemy {
-    int positionX;
-    int positionY;
+    int posX;
+    int posY;
 
-    Enemy(int positionX, int positionY) {
-        this.positionX = positionX;
-        this.positionY = positionY;
+    Enemy(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
     }
 
-    int getPositionX() {
-        return positionX;
+    int get_posX() {
+        return posX;
     }
 
-    int getPositionY() {
-        return positionY;
+    int get_posY() {
+        return posY;
     }
 
     void escape() {
-        positionX = -1;
-        positionY = -1;
+        posX = -1;
+        posY = -1;
     }
 
     void attack(Vehicle player) {
-        int playerX = player.getPositionX();
-        int playerY = player.getPositionY();
+        int playerX = player.get_posX();
+        int playerY = player.get_posY();
 
-        if (Math.abs(playerX - positionX) <= 1 && Math.abs(playerY - positionY) <= 1) {
+        if (Math.abs(playerX - posX) <= 1 && Math.abs(playerY - posY) <= 1) {
             System.out.println("Collision! " + getName() + " attacked " + player.getName() + "!");
         }
     }
