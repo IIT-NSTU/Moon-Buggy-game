1f257f3d97eb7011b20b882d211488b0598b2670
Yarafat2001 <121226676+Yarafat2001@users.noreply.github.com>
2023-08-30 17:47:12 +0600

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,8 +1,6 @@
-class Enemy {
-    int posX;
-    int posY;
-
+class Enemy extends Vehicle {
     Enemy(int posX, int posY) {
+        super("Enemy");
         this.posX = posX;
         this.posY = posY;
     }
@@ -11,27 +9,28 @@ class Enemy {
         return posX;
     }
 
-    int get_posY() {
-        return posY;
-    }
+
 
     void escape() {
         posX = -1;
         posY = -1;
     }
 
-    void attack(Vehicle player) {
-    int playerX = player.get_posX();
-    int playerY = player.get_posY();
-
-    if (Math.abs(playerX - posX) <= 1 && Math.abs(playerY - posY) <= 1) {
-        System.out.println("Collision! " + getName() + " attacked " + player.getName() + "!");
-        player.eliminate(); // Eliminate the player
-    }
-}
-
-
     String getName() {
         return "Enemy";
     }
+
+    boolean attack(Vehicle player) {
+        int playerX = player.get_posX();
+        int playerY = player.get_posY();
+
+        if (Math.abs(playerX - posX) <= 1 && Math.abs(playerY - posY) <= 1) {
+            System.out.println("Collision! " + getName() + " attacked " + player.getName() + "!");
+            player.eliminate(); // Apply the attack effect on the player
+            return true;
+        }
+        return false;
+    }
+
+
 }
